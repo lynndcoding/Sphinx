7bf0996d0799531a972f50c2d2bb615ce8432105
LynnDCoding@outlook.com <LynnDCoding@outlook.com>
2021-10-21 21:14:44 -0500

    Add Race Class

---
+++ Characters/Race.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters
+{
+    public enum Race
+    {
+        Orc,
+        Elf,
+        Human,
+        Gnome,
+        Dwarf,
+        DragonBorn,
+        Troll,
+        DarkElf,
+        Narwhal
+    }//end class
+}//end namespace
