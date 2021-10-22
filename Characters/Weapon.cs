2785b413359366ae74d5d6146b37c371bbdc3694
LynnDCoding@outlook.com <LynnDCoding@outlook.com>
2021-10-21 21:17:51 -0500

    Add Weapon Class

---
+++ Characters/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters
+{
+    public enum Weapon
+    {
+        Longsword,
+        Knife,
+        Dagger,
+        Spear,
+        Bluntstave,
+        Polearm,
+        Axe,
+        Throwingstar,
+        Dartgun,
+        Javelin
+
+    }//end class
+}//end namespace
